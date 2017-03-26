8090fb9b587a9e1e6c4eb5c3ad0658ccab7c35f5
Matthew Moreno <mmore500@gmail.com>
2017-03-26 02:25:24 -0700

    Add git-rebase-todo and *.diff recognition

--- git-commit-atom.go
+++ git-commit-atom.go
@@ -101,7 +101,7 @@ func main(){
     filename := c.Args().Get(0)
 
 
-    if path.Base(filename) == "COMMIT_EDITMSG" || path.Base(filename) == "MERGE_MSG" {
+    if path.Base(filename) == "COMMIT_EDITMSG" || path.Base(filename) == "MERGE_MSG" || path.Base(filename) == "git-rebase-todo" || path.Ext(filename) == ".diff" {
       handle_COMMIT_EDITMSG(filename)
     } else {
       exec.Command("atom", "--wait", filename).Run()
